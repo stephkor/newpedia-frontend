00da6992a5c8346d48f0f9cd7b2c850f588a888c
한 <han@Han-ui-Mac-Book-Air.local>
2020-07-26 19:52:26 +0900

    first commit

---
+++ src/Main.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const Main = () => {
+  return (
+    <div className="Main">
+      <nav>
+      		<ul className="nav_top">
+      			<li></li>
+      			<li></li>
+      			<li></li>
+      		</ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Main;
