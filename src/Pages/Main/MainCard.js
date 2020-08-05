400abd2057227d829a2a3f7fe2acfd3a20138725
Steph_KOR <62464603+stephkor@users.noreply.github.com>
2020-08-05 18:41:50 +0900

    Feature/main (#11)
    * applied mockdata
    
    Co-authored-by: 한 <han@Han-ui-Mac-Book-Air.local>

--- src/Pages/Main/MainCard.js
+++ src/Pages/Main/MainCard.js
@@ -3,24 +3,13 @@ import {useState, useEffect} from 'react';
 
 
 
-const MainCard = () => {
-    
-  const [data, setData] = useState([]);
-   
-  useEffect(() => {
-    fetch('mock.json')
-    .then((res)=> res.json())
-    .then((res) => {
-      setData(res.dic_info)
-    });
-  },[])
-  
+const MainCard = ({data, index}) => {
   return (
-    <div className="MainCard">
+    <div className="MainCard" >
       <div className="card">
-        <h1>단어</h1>
-        <p>이자리에 뜻이 들어가게 됩니다 뜻이 굉장히 길 경우에 어떻게 되는지 궁금해 지는군요 할 공부도 많은데 이딴거나 쓰고있고 수민아 정신차리자</p>
-        <span>여기에 예시가 들어가게 되는 것임. 예시는 한문장이면 되지 않을까</span>
+        <h1>{data && data.word_name}</h1>
+        <p>{data && data.word_description.slice(0,100) + '...'}</p>
+        <span>{data && data.word_example}</span>
       </div>
     </div>
   )
